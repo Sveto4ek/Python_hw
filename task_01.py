105eefb62699616af7bdc0c997d12c4238c6a25b
Sveto4ek <lanasoul.sv@gmail.com>
2020-10-22 19:22:43 +0300

    Задачи к 1 уроку

---
+++ task_01.py
@@ -0,0 +1,13 @@
+qty = 7
+print(qty + 2)
+
+name = 'Sveta'
+print(name)
+
+temp = 36.6
+print(type(temp))
+
+surname = input('Введите вашу фамилию: ')
+age = int(input('Введите ваш возраст: '))
+temp = float(input('Ваша температура: '))
+print(f"Фамилия: {surname}, Возраст: {age}, Температура: {temp}")
---
+++ task_06.py
@@ -0,0 +1,7 @@
+den = 1
+a = float(input('Введите а: '))
+b = float(input('Введите b: '))
+while a < b:
+    a = a * 1.1
+    den += 1
+print(den)
